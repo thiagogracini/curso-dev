790858a634a5fc12e52ec3dd0b0971d75fe8b3eb
Thiago Gracini <dev@thiagogracini.com.br>
2026-02-08 23:41:13 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # curso-dev
+
 Repositório criado para acompanhar o curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Ooooo meu fiiiii...🤣</h1>
+  return <h1>Ooooo meu fiiiii...🤣</h1>;
 }
 
 export default Home;
